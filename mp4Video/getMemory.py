5dbf274e031040cbd1f7025c58dcf4a8e9ce3064
yuting0412 <tmpss95185@gmail.com>
2021-01-03 22:02:32 +0800

    add delete old video

---
+++ mp4Video/getMemory.py
@@ -0,0 +1,14 @@
+import os
+from glob import glob
+import datetime
+def main():
+    all_video= glob("*.mp4")
+    now = datetime.datetime.now()
+    seven_days_ago=now-datetime.timedelta(days=7)
+    seven_days_ago = seven_days_ago.strftime("%Y%m%d%H%M%S")
+    for i in range(len(all_video)):
+        if all_video[i].split(".mp4")[0] <= seven_days_ago:
+            print(all_video[i])
+            os.system('rm %s' %(all_video[i]))
+main()
+
